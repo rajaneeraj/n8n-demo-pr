cf935ef9806fae5f0cd3851f4269312ec4dd8706
Mayank Aggarwal <31075014+mayank953@users.noreply.github.com>
2025-08-29 21:17:07 +0530

    Update main.py
    new endpoint added

--- fastapi-template/main.py
+++ fastapi-template/main.py
@@ -25,6 +25,15 @@ def add_msg(msg_name: str) -> dict[str, MsgPayload]:
 
     return {"message": messages_list[msg_id]}
 
+@app.post("/messages/v1/{msg_name}/")
+def add_msg(msg_name: str) -> dict[str, MsgPayload]:
+    # Generate an ID for the item based on the highest ID in the messages_list
+    msg_id = max(messages_list.keys()) + 1 if messages_list else 0
+    messages_list[msg_id] = MsgPayload(msg_id=msg_id, msg_name=msg_name)
+
+    return {"message": messages_list[msg_id]}
+    return "YES"
+
 
 # Route to list all messages
 @app.get("/messages")
